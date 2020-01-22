a74ea245905f4ea382ae8d911d9c354edd20148c
bak83 <bak.dariusz@gmail.com>
2020-01-22 20:41:27 +0100

    Add project files.

---
+++ RLE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RLE
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string napis = "aaabddddddsssssswesweeeee", rle = "";
+			//int licznik = 1;
+
+			for (int i = 0; i < napis.Length; i++)
+			{
+				int licznik = 1;
+
+				while (i + 1 < napis.Length && napis[i] == napis[i + 1])
+				{
+					licznik++;
+					i++;
+				}
+				rle += napis[i];
+				rle += licznik;
+
+			}
+			Console.WriteLine(rle);
+
+
+		}
+
+	}
+}
